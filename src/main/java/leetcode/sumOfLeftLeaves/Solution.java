6f556c10aec2dfd25ca49ed8513380cdbb1857c7
Viacheslav Ivanov <iamfafan2008@yandex.ru>
2020-10-07 16:19:02 +0300

    New Solution of sum of Left Leaves

--- src/main/java/leetcode/sumOfLeftLeaves/Solution.java
+++ src/main/java/leetcode/sumOfLeftLeaves/Solution.java
@@ -1,5 +1,7 @@
 package leetcode.sumOfLeftLeaves;
 
+import java.util.Stack;
+
 public class Solution {
     public static int sumOfLeftLeaves(TreeNode root){
         if (root == null)
@@ -8,6 +10,26 @@ public class Solution {
             return root.left.val + sumOfLeftLeaves(root.right);
         else
             return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+    public static int sumSecondVersion(TreeNode root)
+    {
+        int sum = 0;
+        boolean isLeft = false;
+        Stack<TreeNode> stackTree = new Stack<>();
+        TreeNode current = root;
+        while(current != null || stackTree.size()>0){
+            while (current != null) {
+                stackTree.push(current);
+                if (current.left == null && current.right == null && isLeft)
+                    sum += current.val;
+                current = current.left;
+                isLeft = true;
+            }
+            current = stackTree.pop();
+            current = current.right;
+            isLeft = false;
+        }
+        return sum;
     }
     public static void main(String[] args){
         TreeNode root;
@@ -20,6 +42,21 @@ public class Solution {
         r1 = new TreeNode(3);
         l1 = new TreeNode(2, l2, r2);
         root = new TreeNode(1, l1, r1);
-        int res = sumOfLeftLeaves(root);
+
+
+
+
+
+        int res1 = sumOfLeftLeaves(root);
+        int res2 = sumSecondVersion(root);
+        System.out.println("First check");
+        System.out.println(res1);
+        System.out.println(res2);
+
+        res1 = sumOfLeftLeaves(l1);
+        res2 = sumSecondVersion(l1);
+        System.out.println("Second check");
+        System.out.println(res1);
+        System.out.println(res2);
     }
 }
