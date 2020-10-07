c300787d4a69b62cbb6aa59992ebd50eebb68f18
ivanov <12345678>
2020-10-08 00:04:01 +0300

    Release of new solution of invert tree.

--- src/main/java/leetcode/invertBinaryTree/Solution.java
+++ src/main/java/leetcode/invertBinaryTree/Solution.java
@@ -19,12 +19,11 @@ public class Solution {
         TreeNode current;
         if (root == null)
             return null;
-        TreeNode invertTree = root;
         Queue<TreeNode> nodeQueue = new ArrayDeque<>();
-        nodeQueue.add(invertTree);
+        nodeQueue.add(root);
         while (!nodeQueue.isEmpty()) {
             current = nodeQueue.poll();
-            TreeNode tmpLeft = current.left;
+            TreeNode tmpLeft = current.right;
             current.right = current.left;
             current.left = tmpLeft;
             if (current.left != null)
@@ -32,21 +31,31 @@ public class Solution {
             if (current.right != null)
                 nodeQueue.add(current.right);
         }
-        return invertTree;
+        return root;
     }
 
     public static void printTree(TreeNode _node)
     {
+        StringBuilder strTree = new StringBuilder();
+        strTree.append(_node.val);
         Queue<TreeNode> nodeQueue = new ArrayDeque<>();
         nodeQueue.add(_node);
         while (!nodeQueue.isEmpty()) {
             TreeNode current = nodeQueue.poll();
-            System.out.println(current.val);
-            if (current != null) {
+            if (current.left != null){
                 nodeQueue.add(current.left);
-                nodeQueue.add(current.right);
+                strTree.append(" ").append(current.left.val);
             }
+            else
+                strTree.append(" null");
+            if(current.right != null){
+                nodeQueue.add(current.right);
+                strTree.append(" ").append(current.right.val);
+            }
+            else
+                strTree.append(" null");
         }
+        System.out.println(strTree);
     }
     public static void main(String[] args){
         TreeNode root1, root2;
@@ -60,9 +69,21 @@ public class Solution {
         l1 = new TreeNode(2, l2, r2);
         root1 = new TreeNode(1, l1, r1);
         root2 = new TreeNode(1, l1, r1);
+        System.out.println("ROOT1 BEFORE");
+        printTree(root1);
+        System.out.println("ROOT2 BEFORE");
+        printTree(root2);
+
         TreeNode res1 = invertTree(root1);
-        TreeNode res2 = invertTree2(root2);
+        System.out.println("\nINVERTING ROOT1");
+        printTree(root1);
+        System.out.println("\nINVERTING RES1");
         printTree(res1);
+
+        TreeNode res2 = invertTree2(root2);
+        System.out.println("\nINVERTING ROOT2");
+        printTree(root2);
+        System.out.println("\nINVERTING RES2");
         printTree(res2);
     }
 }
